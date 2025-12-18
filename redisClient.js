343717d7f610e53dd33f1184e546af39fdca4cf9
chonnapatxd <chonnapatxd@gmail.com>
2025-12-18 16:55:44 +0700

    add username and password to Redis

--- redisClient.js
+++ redisClient.js
@@ -3,6 +3,8 @@ const Redis = require("ioredis");
 const redis = new Redis({
   host: process.env.REDIS_HOST || "127.0.0.1",
   port: process.env.REDIS_PORT || 6379,
+  username: process.env.REDIS_USERNAME || "default", 
+  password: process.env.REDIS_PASSWORD || "mypassword", 
   maxRetriesPerRequest: 1,
 });
 
