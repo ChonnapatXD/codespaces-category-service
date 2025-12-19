b5c10ec7c8074b7e37a1335da405dce598dc87b9
ChonnapatXD <161345739+ChonnapatXD@users.noreply.github.com>
2025-12-19 17:12:04 +0700

    add Username to Redis cache

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const redis = require("./redisClient");
 
 const app = express();
--- redisClient.js
+++ redisClient.js
@@ -1,10 +1,10 @@
 const Redis = require("ioredis");
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: process.env.REDIS_PORT || 6379,
-  username: process.env.REDIS_USERNAME || "default", 
-  password: process.env.REDIS_PASSWORD || "mypassword", 
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
   maxRetriesPerRequest: 1,
 });
 
